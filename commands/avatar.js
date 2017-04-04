d7081b2bc67240377133ce6c732a4afc3afff46f
HasithaAbeysekera <hasithaa@hotmail.com>
2017-04-04 16:46:33 +0800

    -avatar server

--- commands/avatar.js
+++ commands/avatar.js
@@ -8,13 +8,17 @@ exports.run = function(client, message, args) {
     if (message.mentions.users.first()) {
         let target = message.mentions.users.first();
         let targetuser = message.guild.members.find(u => u.id === target.id);
-        userAvatar (targetuser, message);
+        userAvatar(targetuser, message);
     } else
     if (message.guild.members.find(u => u.displayName.toLowerCase() == args.join(" ").toLowerCase())) {
         let target = message.guild.members.find(u => u.displayName.toLowerCase() == args.join(" ").toLowerCase());
-        userAvatar (target, message);
+        userAvatar(target, message);
+    } else
+    if (args == "server") {
+        message.channel.sendMessage(`The server avatar is:`);
+        message.channel.sendFile(message.guild.iconURL, `servericon.jpg`);
     } else {
-        message.channel.sendMessage("User not found");
+        message.channel.sendMessage(`User not found`);
         return;
     }
 };
