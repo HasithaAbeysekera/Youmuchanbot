9c2e52046ef12fbe5eb5516073c9da90cdede435
HasithaAbeysekera <hasithaa@hotmail.com>
2017-04-12 00:49:43 +0800

    Formatted hestiadance

--- commands/hestiadance.js
+++ commands/hestiadance.js
@@ -1,19 +1,19 @@
 exports.run = function(client, message, args) {
     message.channel.sendEmbed({
         color: 3447003,
-        title: `My Shiny Teeth and Me`,
+        title: `:notes:My Shiny Teeth and Me:notes:`,
         url: 'http://hestia.dance/',
         image: {
             url: "http://i.imgur.com/GqaL7Ib.gif"
         },
         fields: [{
                 name: '\u200b',
-                value: 'My shiny teeth that twinkle \n'
-                + 'Just like the stars in space \n'
-                + 'My shiny teeth that sparkle \n'
-                + 'Add beauty to my face \n'
-                + 'My shiny teeth that glisten \n'
-                + 'Just like a Christmas tree \n'
+                value: 'My shiny teeth that twinkle\n'
+                + 'Just like the stars in space\n'
+                + 'My shiny teeth that sparkle\n'
+                + 'Add beauty to my face\n'
+                + 'My shiny teeth that glisten\n'
+                + 'Just like a Christmas tree\n'
                 + 'You know you\'ll walk a mile just to see me smile\n'
                 + 'Whoo!\n'
                 + 'My Shiny Teeth and Me\n'
