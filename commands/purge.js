72e789e3123ebf0b19f15b229789bf57507c950f
HasithaAbeysekera <hasithaa@hotmail.com>
2017-05-13 19:36:21 +0800

    added user option to purge command

--- commands/purge.js
+++ commands/purge.js
@@ -1,37 +1,55 @@
 const Discord = require("discord.js");
 const prefix = require('../config.json').prefix;
+var getUser = require('../util/getUser.js');
+
 exports.run = (client, message, args) => {
     if (!args[0]) {
         return message.channel.sendMessage("Please specify the amount of messages to delete and, if desired, a user");
     } else {
-        if (!args[1]) { //no username specified
-            amount = parseInt(args[0]);
-            if (!amount) {
-                return message.channel.sendMessage("Error: please specify a number of messages to delete");
-            } else {
+        amount = parseInt(args[0]);
+        if (!amount) {
+            return message.channel.sendMessage("Error: please specify a number of messages to delete");
+        } else {
+            if (!args[1]) { //no username specified
                 message.channel.fetchMessages({
                     limit: amount,
                 }).then((messages) => {
                     message.channel.bulkDelete(messages).catch(error => console.log(error.stack));
                 });
+                message.channel.sendMessage(`Deleting last ${amount} messages...`);
+            } else
+            if (message.mentions.users.first()) {
+                target = message.mentions.users.first();
+                message.channel.fetchMessages({
+                    limit: amount,
+                }).then((messages) => {
+                    if (target) {
+                        const filterBy = target ? user.id : Client.user.id;
+                        userMessages = messages.filter(m => m.author.id === filterBy).array().slice(0, amount);
+                    }
+                    message.channel.bulkDelete(userMessages).catch(error => console.log(error.stack));
+                });
+                message.channel.sendMessage(`Deleting last ${amount} messages from user mentioned: ${target}`);
+            } else {
+                target = getUser(message, args[1]);
+                if (!target) {
+                    message.channel.sendMessage(`Error: User not found`);
+                } else {
+                    message.channel.fetchMessages({
+                        limit: amount,
+                    }).then((messages) => {
+                        if (target) {
+                            const filterBy = target ? user.id : Client.user.id;
+                            userMessages = messages.filter(m => m.author.id === filterBy).array().slice(0, amount);
+                        }
+                        message.channel.bulkDelete(userMessages).catch(error => console.log(error.stack));
+                    });
+                    message.channel.sendMessage(`Deleting last ${amount} messages from user: ${target}`);
+                }
             }
         }
     }
 };
-
-// const user = <Message>.mentions.users.first();
-// const amount = !!parseInt(<Message>.content.split(" ")[1]) ? parseInt(<Message>.content.split(" ")[2]) : parseInt(<Message>.content.split(" ")[1])
-// if (!amount) return <Message>.reply("Must specify an amount to delete!");
-// if (!amount && !user) return <Message>.reply("Must specify a user and amount, or just an amount, of messages to purge!");
-// <Message>.channel.fetchMessages({
-//   limit: amount,
-// }).then((messages) => {
-//   if (user) {
-//     const filterBy = user ? user.id : Client.user.id;
-//     messages = messages.filter(m => m.author.id === filterBy).array().slice(0, amount);
-//   }
-//   <Message>.channel.bulkDelete(messages).catch(error => console.log(error.stack));
-// });
 
 exports.conf = {
     enabled: true,
@@ -42,6 +60,6 @@ exports.conf = {
 
 exports.help = {
     name: 'purge',
-    description: 'Removes X messages from the channel. Can specify an optional user to only remove their messages.',
+    description: 'Removes last X messages from the channel. Can specify an optional user to only remove their messages.',
     usage: 'purge [number] [username]'
 };
