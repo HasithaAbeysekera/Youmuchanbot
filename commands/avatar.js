eb6176d8837b245347bb7a36b9597c4c5e28d1a4
HasithaAbeysekera <hasithaa@hotmail.com>
2017-09-20 16:23:18 +0800

    bully protection on -avatar

--- commands/avatar.js
+++ commands/avatar.js
@@ -1,45 +1,51 @@
 const Discord = require("discord.js");
 var getUser = require('../util/getUser.js');
+const owner = require('../config.json').ownerid;
 
 exports.run = function(client, message, args) {
-    if (!args[0]) {
-        message.reply("Your avatar is:");
-        message.channel.send(new Discord.Attachment(message.author.avatarURL, 'avatar.jpg'));
-        // message.channel.send(avatar);
-    } else if (args[0].toLowerCase() === 'server') {
-        serveravatar = message.guild.iconURL;
-        if (serveravatar) {
-            message.channel.send(`The server avatar is:`);
-            message.channel.send(new Discord.Attachment(message.guild.iconURL, 'avatar.jpg'));
-        } else {
-            message.channel.send("This server doesn't have an icon");
-        }
-    } else if (message.mentions.users.first()) {
-        target = message.mentions.users.first();
-        message.channel.send(`${target}'s avatar is:`);
-        message.channel.send(new Discord.Attachment(target.avatarURL, 'avatar.jpg'));
-    } else if (args[0]) {
-        target = getUser(message, args[0]);
-        if (target) {
-          message.channel.send(`${target}'s avatar is:`);
-          message.channel.send(new Discord.Attachment(target.avatarURL, 'avatar.jpg'));
-        } else {
-            message.channel.send(`Error: User not found`);
-        }
+  if (!args[0]) {
+    message.reply("Your avatar is:");
+    return message.channel.send(new Discord.Attachment(message.author.avatarURL, 'avatar.jpg'));
+  } else if (args[0].toLowerCase() === 'server') {
+    serveravatar = message.guild.iconURL;
+    if (serveravatar) {
+      message.channel.send(`The server avatar is:`);
+      return message.channel.send(new Discord.Attachment(message.guild.iconURL, 'avatar.jpg'));
     } else {
-        return message.channel.send(`Error: incorrect format`);
+      return message.channel.send("This server doesn't have an icon");
     }
+  } else if (message.mentions.users.first()) {
+    target = message.mentions.users.first();
+    message.channel.send(`${target}'s avatar is:`);
+    if (target.id == owner && message.author.id != owner) {
+      return message.channel.send("FUCK OFF JAMES");
+    }
+    return message.channel.send(new Discord.Attachment(target.avatarURL, 'avatar.jpg'));
+  } else if (args[0]) {
+    target = getUser(message, args[0]);
+    if (target) {
+      if (target.id == owner && message.author.id != owner) {
+        message.channel.send(`${target}'s avatar is:`);
+        return message.channel.send("FUCK OFF JAMES");
+      }
+      return message.channel.send(new Discord.Attachment(target.avatarURL, 'avatar.jpg'));
+    } else {
+      return message.channel.send(`Error: User not found`);
+    }
+  } else {
+    return message.channel.send(`Error: incorrect format`);
+  }
 };
 
 exports.conf = {
-    enabled: true,
-    guildOnly: false,
-    aliases: ['profilepic', 'icon'],
-    permLevel: 0
+  enabled: true,
+  guildOnly: false,
+  aliases: ['profilepic', 'icon'],
+  permLevel: 0
 };
 
 exports.help = {
-    name: 'avatar',
-    description: 'Display\'s the user\'s or server\'s avatar. Displays your own avatar if username is empty',
-    usage: 'avatar [username (plaintext or mention)] or [server]'
+  name: 'avatar',
+  description: 'Display\'s the user\'s or server\'s avatar. Displays your own avatar if username is empty',
+  usage: 'avatar [username (plaintext or mention)] or [server]'
 };
